3fc27e04a8099a60ae90da7591b1d9502f275a34
João Fábio Marques <jfmdev4@gmail.com>
2025-07-11 16:49:34 -0300

    Aula 02 Concluída

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,56 +1,17 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { useQuery } from "@tanstack/react-query"
-import { ArrowRight } from "lucide-react"
-import { Link } from "react-router-dom"
-
-type GetRoomsAPIResponse = Array <{
-    id: string
-    name: string
-    questionsCount: number
-    createdAt: string
-}>
+import { CreateRoomForm } from "@/components/create-room-form";
+import { RoomList } from "@/components/room-list";
 
 export function CreateRoom(){
 
-    const {data, isLoading} = useQuery({
-        queryKey: ['get-rooms'],
-        queryFn: async() => {
-            const response = await fetch('http://localhost:3333/rooms')
-            const result: GetRoomsAPIResponse = await response.json()
-
-            return result
-        },
-    })
+    
 
     return (
     <div className="min-h-screen px-4 py-8">
         <div className="mx-auto max-w-4xl">
             <div className="grid grid-cols-2 items-start gap-8">
                 <div/>
-                <Card>
-                    <CardHeader>
-                        <CardTitle>
-                            Salas recentes
-                        </CardTitle>
-                        <CardDescription>
-                            Acesso rápido para as salas criadas recentemente
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent className="flex flex-col gap-3">
-                        {data?.map(room => {
-                            return <div key = {room.id}
-                                className="flex items-center justify-between rounded-lg border p-3 hover:bg-accent">
-                                <div className="flex-1">
-                                    <h3 className="font-medium">{room.name}</h3>
-                                </div>
-                                <span>
-                                    Entrar
-                                    <ArrowRight />
-                                </span>
-                            </div>
-                        })}
-                    </CardContent>
-                </Card>
+                <CreateRoomForm />
+                <RoomList />
             </div>
         </div>
     </div>
