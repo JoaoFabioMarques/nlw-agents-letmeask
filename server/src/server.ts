4e29ddf5067713c1357188cf9f9cfd079b1202c8
João Fábio Marques <jfmdev4@gmail.com>
2025-07-12 22:40:16 -0300

    Aula 03 Concluída

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,21 @@
 import { fastify } from 'fastify'
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
 import {fastifyCors} from '@fastify/cors'
+import {fastifyMultipart} from '@fastify/multipart'
 import { env } from './env.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 import { createQuestionRoute } from './http/routes/create-question.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.register(fastifyCors,{
     origin: 'http://localhost:5173'
 })
+
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -21,5 +26,6 @@ app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestions)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({port: env.PORT})
